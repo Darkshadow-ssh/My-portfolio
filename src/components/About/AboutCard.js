d829cda92a37b9afcc37ea6e95aa6e17a4ee427c
Sarthak Chauhan <119244504+Darkshadow-ssh@users.noreply.github.com>
2025-05-13 12:08:56 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function AboutCard() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//cdn.credly.com/assets/utilities/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <Card className="quote-card-view">
       <Card.Body>
@@ -17,12 +24,28 @@ function AboutCard() {
             <br />
             I have successfully completed CompTIA Network+ and CompTIA Security+ Certification.
             <br />
-              <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="bc7afd4f-0c10-4a2d-8866-4e049ee3db22" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-              <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="5082d455-c9d8-442f-8a27-47f517511c35" data-share-badge-host="https://www.credly.com"></div><script type="text/javascript" async src="//cdn.credly.com/assets/utilities/embed.js"></script>
-              
-            <br />
+          </p>
+
+          {/* 👇 Add Credly badges here */}
+          <div style={{ display: "flex", gap: "1rem", marginBottom: "1rem" }}>
+            <div
+              data-iframe-width="150"
+              data-iframe-height="270"
+              data-share-badge-id="bc7afd4f-0c10-4a2d-8866-4e049ee3db22"
+              data-share-badge-host="https://www.credly.com"
+            ></div>
+            <div
+              data-iframe-width="150"
+              data-iframe-height="270"
+              data-share-badge-id="5082d455-c9d8-442f-8a27-47f517511c35"
+              data-share-badge-host="https://www.credly.com"
+            ></div>
+          </div>
+
+          <p style={{ textAlign: "justify" }}>
             Apart from coding, some other activities that I love to do!
           </p>
+
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing CTF Challenges
